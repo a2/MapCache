7b90d419f890ac9b71495fff794189369d7b972a
Alexsander Akers <me@a2.io>
2021-10-03 22:18:39 -0400

    Make ZoomableTile mutable, initializable

--- MapCache/Classes/ZoomableTile.swift
+++ MapCache/Classes/ZoomableTile.swift
@@ -12,14 +12,14 @@ import MapKit
 /// Specifies a single tile and area of the tile that should upscaled.
 ///
 public struct ZoomableTile {
-    
+
     /// Path for the tile with `maximumZ` supported by the tile server set in the config.
     /// This is the path with the best resolution tile from wich this zoomable tile can be interpolated.
     /// - SeeAlso: `MapCacheConfig``
-    let maximumZPath: MKTileOverlayPath
+    public var maximumZPath: MKTileOverlayPath
     
     /// Rectangle area ocupied by this tile
-    let rect: MKMapRect
+    public var rect: MKMapRect
     
     /// Scale over the tile of the maximumZ path.
     /// It is a multiple of 2 (2, 4, 8).
@@ -27,6 +27,11 @@ public struct ZoomableTile {
     /// one axis. For example,  overZoom=4 means that in each axis the tile is divided in 4 as well as
     /// in the Y axis. So, the original tile at maximumZ is divided in 16 subtiles.
     /// The `rect`tells us, among those tiles, which one is this tile.
-    let overZoom: Int
-}
+    public var overZoom: Int
 
+    public init(maximumZPath: MKTileOverlayPath, rect: MKMapRect, overZoom: Int) {
+        self.maximumZPath = maximumZPath
+        self.rect = rect
+        self.overZoom = overZoom
+    }
+}
